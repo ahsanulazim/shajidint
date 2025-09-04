5787a3b36d54227783ec76e83cee41996c64daab
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-09-04 23:18:34 +0600

    Side Bar Customized

--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -58,13 +58,13 @@ export default function Navbar() {
 
                 </div>
             </div>
-            <div className="drawer-side">
+            <menu className="drawer-side">
                 <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
                 <ul className="menu bg-base-200 min-h-full w-80 p-4">
                     {/* Sidebar content here */}
                     {nav}
                 </ul>
-            </div>
+            </menu>
         </header>
     )
 }
