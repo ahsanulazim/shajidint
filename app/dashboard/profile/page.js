d3ee30a6de79daca5de8a9a942b3ab4ed85069a6
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-09-16 18:27:10 +0600

    profile page is responsive

--- app/dashboard/profile/page.js
+++ app/dashboard/profile/page.js
@@ -1,12 +1,16 @@
 import Breadcrumbs from "@/Components/dashboard/Breadcrumbs";
-import { FaFacebook, FaLinkedin, FaPenToSquare, FaSquareXTwitter } from "react-icons/fa6";
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaPenToSquare,
+  FaSquareXTwitter,
+} from "react-icons/fa6";
 
 export default function page() {
   return (
     <>
       <Breadcrumbs title="Profile" />
       <div>
-
         <h1 className="font-medium text-2xl sm:text-4xl tracking-tighter">
           Profile
         </h1>
@@ -14,9 +18,9 @@ export default function page() {
           Your Personal Informations
         </p>
       </div>
-      <div className="flex flex-col md:flex-row gap-10 mt-5 items-center bg-white p-8 rounded-md shadow-sm max-w-fit">
+      <div className="flex flex-col md:flex-row gap-10 mt-5 items-center bg-white p-8 rounded-md shadow-sm sm:max-w-fit">
         <div className="avatar">
-          <div className="ring-main ring-offset-base-100 w-56 rounded-full ring-2 ring-offset-2">
+          <div className="ring-main ring-offset-base-100 w-full max-w-56 rounded-full ring-2 ring-offset-2">
             <img
               className="max-w-56"
               src="/plabon.jpg"
@@ -25,25 +29,34 @@ export default function page() {
           </div>
         </div>
 
-        <fieldset className="fieldset w-xs">
-          <div className="flex items-center gap-x-5 mb-5">
-            <h1 className="text-3xl tracking-tighter whitespace-nowrap">Personal Information</h1>
-            <button className="btn rounded-md btn-neutral"><FaPenToSquare />Edit</button>
-          </div>
+        <fieldset className="fieldset">
+          <h1 className="text-2xl sm:text-3xl tracking-tighter md:whitespace-nowrap mb-5">
+            Personal Information
+          </h1>
           <label className="label">Name</label>
-          <p className="text-lg">Ahsanul Azim Plabon</p>
+          <p className="text-md sm:text-lg">Ahsanul Azim Plabon</p>
           <label className="label">Email</label>
-          <p className="text-lg">ahsanulazim6@gmail.com</p>
+          <p className="text-md sm:text-lg">ahsanulazim6@gmail.com</p>
           <label className="label">Phone</label>
-          <p className="text-lg">01612345678</p>
+          <p className="text-md sm:text-lg">01612345678</p>
           <label className="label">Designation</label>
-          <p className="text-lg">Founder & CEO</p>
+          <p className="text-md sm:text-lg">Founder & CEO</p>
           <label className="label">Social Accounts</label>
           <div className="flex gap-x-2">
-            <a href="#" className="text-xl"><FaFacebook /></a>
-            <a href="#" className="text-xl"><FaLinkedin /></a>
-            <a href="#" className="text-xl"><FaSquareXTwitter /></a>
+            <a href="#" className="text-xl">
+              <FaFacebook />
+            </a>
+            <a href="#" className="text-xl">
+              <FaLinkedin />
+            </a>
+            <a href="#" className="text-xl">
+              <FaSquareXTwitter />
+            </a>
           </div>
+          <button className="btn rounded-md btn-neutral mt-5">
+            <FaPenToSquare />
+            Edit
+          </button>
         </fieldset>
       </div>
     </>
