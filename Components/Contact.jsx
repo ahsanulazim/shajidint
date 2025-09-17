6eb17fa1700cf6d1b2b6b582bd508acd2187e955
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-09-17 16:06:38 +0600

    contact section timeline

--- Components/Contact.jsx
+++ Components/Contact.jsx
@@ -6,12 +6,92 @@ export default function Contact() {
             <div>
                 <h2 className="font-bold text-4xl lg:text-5xl tracking-tighter mb-5 text-balance">Ready to take your marketing to the next level?</h2>
                 <p className="lg:text-lg/8 font-bold">Get a look at:</p>
-                <ul className="pl-5 opacity-70 lg:text-base/8">
+                {/* <ul className="pl-5 opacity-70 lg:text-base/8">
                     <li className="list-disc">How NinjaPromo works</li>
                     <li className="list-disc">How you can do marketing at scale better, faster and cheaper</li>
                     <li className="list-disc">How we&apos;re different from agencies, freelancers and in-house marketing teams (hint: we&apos;re 15x faster than hiring!)</li>
                     <li className="list-disc">The most suitable subscription plan for your needs</li>
-                </ul>
+                </ul> */}
+                <div className="flex justify-start">
+
+                    <ul className="timeline timeline-vertical items-start">
+                        <li className="!grid-cols-none">
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end timeline-box">How Shajidint works</div>
+                            <hr />
+                        </li>
+                        <li className="!grid-cols-none">
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end timeline-box">How you can do marketing at scale better, faster and cheaper</div>
+                            <hr />
+                        </li>
+                        <li className="!grid-cols-none">
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end timeline-box">How we&apos;re different from agencies, freelancers and in-house marketing teams (hint: we&apos;re 15x faster than hiring!)</div>
+                            <hr />
+                        </li>
+                        <li className="!grid-cols-none">
+                            <hr />
+                            <div className="timeline-middle">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                    className="h-5 w-5"
+                                >
+                                    <path
+                                        fillRule="evenodd"
+                                        d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                                        clipRule="evenodd"
+                                    />
+                                </svg>
+                            </div>
+                            <div className="timeline-end timeline-box">The most suitable subscription plan for your needs</div>
+                        </li>
+
+                    </ul>
+                </div>
             </div>
             <ContactFrom />
         </section>
