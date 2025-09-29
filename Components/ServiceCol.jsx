ea77542a12832cd902400ee20b748728081fcb0b
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-09-29 13:29:50 +0600

    services gallery

--- Components/ServiceCol.jsx
+++ Components/ServiceCol.jsx
@@ -1,3 +1,54 @@
 export default function ServiceCol() {
-  return <div>ServiceCol</div>;
+  return <>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (1).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (2).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (3).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (4).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (5).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (6).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (7).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (8).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (9).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (10).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (11).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (12).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (13).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (14).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (15).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+      <div className="p-2 rounded-md border border-base-200">
+        <img src="/service/army (16).jpg" alt="army" className="w-full aspect-video object-cover" />
+      </div>
+    </div>
+  </>;
 }
