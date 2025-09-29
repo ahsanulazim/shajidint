c6b7d6112f1a66694c38ab92ef311365f4bb74ac
Ahsanul Azim Plabon <ahsanulazim6@gmail.com>
2025-09-29 13:31:50 +0600

    services gallery image component

--- Components/ServiceCol.jsx
+++ Components/ServiceCol.jsx
@@ -1,53 +1,55 @@
+import Image from "next/image";
+
 export default function ServiceCol() {
   return <>
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2">
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (1).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (1).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (2).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (2).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (3).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (3).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (4).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (4).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (5).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (5).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (6).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (6).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (7).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (7).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (8).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (8).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (9).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (9).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (10).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (10).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (11).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (11).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (12).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (12).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (13).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (13).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (14).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (14).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (15).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (15).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
       <div className="p-2 rounded-md border border-base-200">
-        <img src="/service/army (16).jpg" alt="army" className="w-full aspect-video object-cover" />
+        <Image width={900} height={500} src="/service/army (16).jpg" alt="army" className="w-full aspect-video object-cover" />
       </div>
     </div>
   </>;
